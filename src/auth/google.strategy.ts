dadd9fe8f6b8d51aa4666a697640e91cee7d5d80
Raoul1996 <baobaozhou199606@gmail.com>
2020-05-29 02:49:40 +0800

    remove google oauth dep

--- src/auth/google.strategy.ts
+++
@@ -1,9 +0,0 @@
-import { PassportStrategy } from '@nestjs/passport';
-import {Strategy} from 'passport-google-oauth20'
-export class GoogleStrategy extends PassportStrategy(Strategy,'google'){
-constructor() {
-  super({
-
-  });
-}
-}
